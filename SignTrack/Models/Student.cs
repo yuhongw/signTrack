6d356e325a8740b467813e372e2464b3b0a9669f
Bruce Yu <yuhon@live.com>
2017-04-21 06:59:48 +0800

    init project

---
+++ SignTrack/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignTrack.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(10)]
+        public string Name { get; set; }
+        [MaxLength(20)]
+        public string Phone { get; set; }
+        [MaxLength(30)]
+        public string PhoneId { get; set; }
+       
+    }
+}
---
+++ SignTrack/SignContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using MySQL.Data.EntityFrameworkCore.Extensions;
+using SignTrack.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignTrack
+{
+    public class SignContext:DbContext
+    {
+        public SignContext(DbContextOptions<SignContext> options) : base(options)
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+
+    public static class SignContextFactory
+    {
+        public static SignContext Create()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<SignContext>();
+            optionsBuilder.UseMySQL("server=localhost;userid=root;pwd=1111;database=yu");
+            var context = new SignContext(optionsBuilder.Options);
+            context.Database.EnsureCreated();
+            return context;
+        }
+    }
+}
